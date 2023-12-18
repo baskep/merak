fcc0d4ba9a3987619767efd95d79f82c9174d927
baskep <xtidcr@outlook.com>
2023-12-18 14:29:17 +0800

    feat: 测试创建prisma 工具

--- src/lib/prisma.jsx
+++ src/lib/prisma.jsx
@@ -2,10 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query'] })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
