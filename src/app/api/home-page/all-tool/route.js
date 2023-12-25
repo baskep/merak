6ff63ebeec5e15f1abd423daeb4672acd4e96579
baskep <xtidcr@outlook.com>
2023-12-25 15:38:25 +0800

    feat: 联调prisma 后端

--- src/app/api/home-page/all-tool/route.js
+++ src/app/api/home-page/all-tool/route.js
@@ -1,12 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
+import '@/utils/patch'
+
 export async function GET() {
   try {
     const prisma = new PrismaClient()
     const res = await prisma.category.findMany()
-    console.log(res)
-    return NextResponse.json({ code: 200, message: '', data: {} })
+    return NextResponse.json({ code: 200, message: '', data: res })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ code: 500, message: '请示失败，请稍后再试', data: {} })
